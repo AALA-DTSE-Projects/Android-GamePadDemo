8b144767537633cef7d0974ae3f707d37e921c21
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-05-13 20:03:15 +0900

    Add Service to connect with HOS app

---
+++ app/src/main/aidl/jp/huawei/a2hdemo/IGameInterface.aidl
@@ -0,0 +1,5 @@
+package jp.huawei.a2hdemo;
+
+interface IGameInterface {
+    void action(String deviceId, String action);
+}
---
+++ app/src/main/java/jp/huawei/a2hdemo/local/HandleEvent.kt
@@ -0,0 +1,6 @@
+package jp.huawei.a2hdemo.local
+
+data class HandleEvent(
+    val deviceId: String,
+    val action: String
+)
